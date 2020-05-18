d625f8853c73a948988aeeff2562f3cf72c211e5
S10N <sion.jeremiah@gmail.com>
2020-05-18 14:33:19 +0000

    test

--- main.js
+++ main.js
@@ -9,8 +9,12 @@ function setData(jsonData){
 }
     
     
-    hr.onreadystatechange = function() {
+    xhr.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200){
         setData(JSON.parse(this.responseText));
     }
+
+    setTimeout(function(){
+        console.log(data);
+    }, 500);
 };
