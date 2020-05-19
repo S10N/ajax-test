c28edcc9d8704e037dfd934238949d19e03140ee
S10N <sion.jeremiah@gmail.com>
2020-05-19 08:23:11 +0000

    19.5.20 09.22 first commit

--- main.js
+++ main.js
@@ -13,4 +13,8 @@ function setData(jsonData){
     if (this.readyState == 4 && this.status == 200){
         setData(JSON.parse(this.responseText));
     }
+
+    setTimeout(function(){
+        console.log(data);
+        }, 500);
 };
