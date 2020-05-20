d4f6b40ba8d9956aba7dbb386303c06ebe439946
S10N <sion.jeremiah@gmail.com>
2020-05-20 10:48:17 +0000

    200520 first commit for today changed js file var = el added

--- main.js
+++ main.js
@@ -1,23 +1,27 @@
-const baseUrl = "https://ci-swapi.herokuapp.com/api/";
+const baseURL = "https://ci-swapi.herokuapp.com/api/";
 
 function getData(type, cb) {
-  var xhr = new XMLHttpRequest();
+    var xhr = new XMLHttpRequest();
 
-  xhr.open("GET", baseUrl + type + "/");
-  xhr.send();
+    xhr.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            cb(JSON.parse(this.responseText));
+        }
+    };
 
-  xhr.onreadystatechange = function () {
-    if (this.readystate == 4 && this.status == 200) {
-      cb(JSON.parse(this.responseText));
-    }
-  };
+    xhr.open("GET", baseURL + type + "/");
+    xhr.send();
 }
+
 function writeToDocument(type) {
-  getData(type, function (data) {
-      data = data.results;
-      data.forEach(function(item) {
-      document.getElementById("data").innerHTML = item.name;    
-      });
-    
-  });
+    var el = document.getElementById("data");
+    el.innerHTML = "";
+
+    getData(type, function(data) {
+        data = data.results;
+
+        data.forEach(function(item) {
+            el.innerHTML += "<p>" + item.name + "</p>";
+        });
+    });
 }
